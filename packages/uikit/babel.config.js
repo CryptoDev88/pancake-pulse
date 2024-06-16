22450b7d1977f8bcd86573e3b809ce6910838a87
CryptoDev88 <165830499+CryptoDev88@users.noreply.github.com>
2024-06-16 20:09:21 +0900

    Revert "node module mismatching issue solve"
    This reverts commit a699533b753c55608ba377f986f9a16803be4f7b.

--- packages/uikit/babel.config.js
+++ packages/uikit/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   presets: ["@babel/preset-env", ["@babel/preset-react", { runtime: "automatic" }]],
-  plugins: ["styled-components", "@babel/plugin-proposal-optional-chaining"],
+  plugins: ["styled-components", "@vanilla-extract/babel-plugin"],
 };
